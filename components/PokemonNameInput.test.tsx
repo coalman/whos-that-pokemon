1a40d9fa7ef5b658f46baef96ef833a779d4d59f
Michael Coleman <tehcoalman@gmail.com>
2022-09-20 11:22:36 -0400

    added more name input tests
    Found a crash with {ArrowDown}{Enter} when there are no choices
    (selects undefined).

--- components/PokemonNameInput.test.tsx
+++ components/PokemonNameInput.test.tsx
@@ -6,9 +6,11 @@ import PokemonNameInput from "./PokemonNameInput";
 const Test = (props: { choices: readonly string[] }) => {
   const [guessEnabled, setGuessEnabled] = useState(true);
   const [guess, setGuess] = useState("");
+  const [committedGuess, setCommittedGuess] = useState("");
 
   return (
     <Fragment>
+      <span data-testid="guess">{committedGuess}</span>
       <PokemonNameInput
         guessEnabled={guessEnabled}
         pokemonList={props.choices}
@@ -17,6 +19,7 @@ const Test = (props: { choices: readonly string[] }) => {
         onGuess={(pokemonName) => {
           setGuessEnabled(false);
           setGuess(pokemonName);
+          setCommittedGuess(pokemonName);
         }}
       />
       <button
@@ -30,6 +33,16 @@ const Test = (props: { choices: readonly string[] }) => {
     </Fragment>
   );
 };
+
+it("should not enter invalid choice", async () => {
+  render(<Test choices={["p1", "p2", "p3"]} />);
+
+  const input = screen.getByDisplayValue("") as HTMLInputElement;
+
+  userEvent.type(input, "p4{Enter}");
+  await waitFor(() => expect(input.value).toBe("p4"));
+  expect(screen.getByTestId("guess").textContent).toBe("");
+});
 
 it("should select the first choice (with keyboard)", async () => {
   render(<Test choices={["p1", "p2", "p3"]} />);
@@ -52,7 +65,7 @@ it("should select the first choice (with mouse)", async () => {
   userEvent.type(input, "2");
   await waitFor(() => expect(input.value).toBe("2"));
 
-  const { [0]: firstOption } = screen.getAllByRole("option");
+  const [firstOption] = screen.getAllByRole("option");
   userEvent.click(firstOption);
 
   await waitFor(() => expect(input.value).toBe("p2"));
@@ -69,4 +82,88 @@ it("should clear auto complete choices on Reset button click", async () => {
   expect(screen.getAllByRole("option")).toHaveLength(1);
   userEvent.click(screen.getByText("Reset"));
   await waitFor(() => expect(screen.queryAllByRole("option")).toHaveLength(0));
+});
+
+it("should not show choices for empty string", async () => {
+  render(<Test choices={["p1", "p2", "p3"]} />);
+
+  const input = screen.getByDisplayValue("") as HTMLInputElement;
+
+  userEvent.type(input, "p");
+  await waitFor(() => expect(input.value).toBe("p"));
+
+  userEvent.type(input, "{Backspace}");
+  await waitFor(() => expect(input.value).toBe(""));
+
+  expect(screen.queryAllByRole("option")).toHaveLength(0);
+});
+
+describe("option selection", () => {
+  async function setup() {
+    render(<Test choices={["p1", "p2", "p3"]} />);
+
+    const input = screen.getByDisplayValue("") as HTMLInputElement;
+
+    // get all 3 choices to appear ("p" is a prefix for all 3 of them)
+    userEvent.type(input, "p");
+    await waitFor(() => expect(input.value).toBe("p"));
+
+    const options = screen.getAllByRole("option");
+    expect(options).toHaveLength(3);
+
+    const getSelectedState = (elements = options) =>
+      elements.map((o) => o.getAttribute("aria-selected") === "true");
+
+    return { input, options, getSelectedState };
+  }
+
+  it("should deselect on {ArrowUp}", async () => {
+    const { input, getSelectedState } = await setup();
+
+    // focus first option
+    userEvent.type(input, "{ArrowDown}");
+    await waitFor(() =>
+      expect(getSelectedState()).toStrictEqual([true, false, false])
+    );
+
+    // unfocus first option
+    userEvent.type(input, "{ArrowUp}");
+    await waitFor(() =>
+      expect(getSelectedState()).toStrictEqual([false, false, false])
+    );
+  });
+
+  it("should deselect on text type", async () => {
+    const { input, getSelectedState } = await setup();
+
+    // focus first option
+    userEvent.type(input, "{ArrowDown}");
+    await waitFor(() =>
+      expect(getSelectedState()).toStrictEqual([true, false, false])
+    );
+
+    // change the choice list to only list ["p2"]. This should also reset focus.
+    userEvent.type(input, "2");
+    await waitFor(() => {
+      const options = screen.getAllByRole("option");
+      expect(options).toHaveLength(1);
+      expect(getSelectedState(options)).toStrictEqual([false]);
+    });
+  });
+
+  it("should not go past last option on {ArrowDown}", async () => {
+    const { input, getSelectedState } = await setup();
+
+    // focus second to last option
+    userEvent.type(input, "{ArrowDown}{ArrowDown}");
+    await waitFor(() =>
+      expect(getSelectedState()).toStrictEqual([false, true, false])
+    );
+
+    // try to go past last option
+    userEvent.type(input, "{ArrowDown}{ArrowDown}");
+    await waitFor(() =>
+      expect(getSelectedState()).toStrictEqual([false, false, true])
+    );
+  });
 });
