03790b75875acf806266d774b6c1306c86c0dd94
Michael Coleman <tehcoalman@gmail.com>
2022-09-19 23:13:30 -0400

    add jest test

---
+++ components/PokemonNameInput.test.tsx
@@ -0,0 +1,37 @@
+import { Fragment, useState } from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import PokemonNameInput from "./PokemonNameInput";
+
+const Test = (props: { choices: readonly string[] }) => {
+  const [guessEnabled, setGuessEnabled] = useState(true);
+  const [guess, setGuess] = useState("");
+
+  return (
+    <Fragment>
+      <PokemonNameInput
+        guessEnabled={guessEnabled}
+        pokemonList={props.choices}
+        value={guess}
+        onChange={setGuess}
+        onGuess={(pokemonName) => {
+          setGuessEnabled(false);
+          setGuess(pokemonName);
+        }}
+      />
+    </Fragment>
+  );
+};
+
+it("should select the first choice after {ArrowDown}{Enter} keys", async () => {
+  render(<Test choices={["p1", "p2", "p3"]} />);
+
+  const input = screen.getByDisplayValue("") as HTMLInputElement;
+
+  userEvent.type(input, "2");
+  await waitFor(() => expect(input.value).toBe("2"));
+
+  userEvent.type(input, "{ArrowDown}{Enter}");
+
+  await waitFor(() => expect(input.value).toBe("p2"));
+});
