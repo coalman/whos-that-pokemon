04c8e99e4e55ab146e3409b7775341407ec540cb
Michael Coleman <tehcoalman@gmail.com>
2022-10-15 13:42:59 -0400

    add api data validation for pokemon indexes (gen 1 only).

--- pages/api/guess.ts
+++ pages/api/guess.ts
@@ -11,11 +11,37 @@ export default async function handler(
 
   const { actualPokemon, guessedPokemon } = req.body;
 
-  // TODO: validate data from req.body
+  try {
+    validatePokemonIndex(actualPokemon);
+    validatePokemonIndex(guessedPokemon);
+  } catch (error: unknown) {
+    let message = "";
+    if (error instanceof Error) {
+      message = error.message;
+    } else {
+      // Probably should just send "unknown error" and log this.
+      message = String(error);
+    }
+
+    res.status(500).send(message);
+    return;
+  }
 
   const guess = await prisma.guess.create({
     data: { actualPokemon, guessedPokemon },
   });
 
   res.status(200).json(guess);
+}
+
+function validatePokemonIndex(value: unknown) {
+  if (typeof value !== "number") {
+    throw new Error(
+      `Pokemon index must be a number (unexpected type: ${typeof value}).`
+    );
+  } else if (value < 0) {
+    throw new Error(`Pokemon index should be positive (value: ${value}).`);
+  } else if (value >= 151) {
+    throw new Error(`Pokemon index should be below 151 (value: ${value}).`);
+  }
 }
