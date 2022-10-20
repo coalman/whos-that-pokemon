8a64cc93f1c2fe18bff9fba5b1231acbf43afeda
Michael Coleman <tehcoalman@gmail.com>
2022-10-19 23:23:33 -0400

    add tests for useRandomPokemon logic

---
+++ lib/useRandomPokemon.test.ts
@@ -0,0 +1,127 @@
+import {
+  indexArray,
+  nextRandomPokemonState,
+  initialRandomPokemonState,
+  startRandomPokemonState,
+  type RandomPokemonState,
+} from "./useRandomPokemon";
+
+describe("indexArray", () => {
+  it("should return empty array for 0 length", () => {
+    expect(indexArray(0)).toHaveLength(0);
+  });
+
+  it.each([
+    [[0], 1],
+    [[0, 1, 2], 3],
+    [[0, 1, 2, 3, 4, 5], 6],
+  ])("should return %s for length=%s", (expected, length) => {
+    expect(indexArray(length)).toStrictEqual(expected);
+  });
+});
+
+describe(initialRandomPokemonState.name, () => {
+  it("should be deterministic", () => {
+    for (let _i = 0; _i < 5; _i++) {
+      const first = initialRandomPokemonState(20);
+      const second = initialRandomPokemonState(20);
+
+      expect(first).toStrictEqual(second);
+    }
+  });
+});
+
+describe(startRandomPokemonState.name, () => {
+  // NOTE: this case only happens in StrictMode because the start effect runs twice.
+  it("should not modify state that has already started (StrictMode check)", () => {
+    let firstState = initialRandomPokemonState(5);
+    firstState = startRandomPokemonState(0)(firstState);
+    let secondState = { ...firstState };
+    secondState = startRandomPokemonState(0.99)(secondState);
+
+    expect(firstState).toStrictEqual(secondState);
+    // also test that it returns the previous state (to bail out of another react render).
+    expect(firstState).toBe(startRandomPokemonState(0.99)(firstState));
+  });
+});
+
+describe(nextRandomPokemonState.name, () => {
+  const init = (length: number, random = 0) => {
+    let state = initialRandomPokemonState(length);
+    state = startRandomPokemonState(random)(state);
+    return state;
+  };
+
+  it("should append correct answer to streak array", () => {
+    let state = init(5);
+    state = nextRandomPokemonState(0.99, true)(state);
+    expect(state).toStrictEqual({
+      currentIndex: 3,
+      indexes: [1, 2, 3, 4],
+      streakIndexes: [0],
+      answeredIndexes: [],
+    });
+  });
+
+  it("should append incorrect answer to answered array", () => {
+    let state = init(5);
+    state = nextRandomPokemonState(0.99, false)(state);
+    expect(state).toStrictEqual({
+      currentIndex: 3,
+      indexes: [1, 2, 3, 4],
+      streakIndexes: [],
+      answeredIndexes: [0],
+    });
+  });
+
+  it("should append streak questions to incorrect questions on wrong answer", () => {
+    let state: RandomPokemonState = {
+      currentIndex: 0,
+      indexes: [0, 1, 2],
+      answeredIndexes: [3, 4],
+      streakIndexes: [5, 6],
+    };
+    state = nextRandomPokemonState(0.5, false)(state);
+
+    expect(state).toStrictEqual({
+      currentIndex: 1,
+      indexes: [1, 2],
+      answeredIndexes: [3, 4, 0, 5, 6],
+      streakIndexes: [],
+    });
+  });
+
+  it("should serve incorrectly answered questions after initial set is finished", () => {
+    let state: RandomPokemonState = {
+      currentIndex: 0,
+      indexes: [0],
+      answeredIndexes: [1, 2, 3],
+      streakIndexes: [4, 5],
+    };
+    state = nextRandomPokemonState(0.5, true)(state);
+
+    expect(state).toStrictEqual({
+      currentIndex: 1,
+      indexes: [1, 2, 3],
+      answeredIndexes: [],
+      streakIndexes: [4, 5, 0],
+    });
+  });
+
+  it("should serve streak questions if last question in a set is incorrectly answered", () => {
+    let state: RandomPokemonState = {
+      currentIndex: 0,
+      indexes: [0],
+      answeredIndexes: [1, 2, 3],
+      streakIndexes: [4, 5],
+    };
+    state = nextRandomPokemonState(0.5, false)(state);
+
+    expect(state).toStrictEqual({
+      currentIndex: 3,
+      indexes: [1, 2, 3, 0, 4, 5],
+      answeredIndexes: [],
+      streakIndexes: [],
+    });
+  });
+});
--- lib/useRandomPokemon.ts
+++ lib/useRandomPokemon.ts
@@ -1,12 +1,9 @@
 import { useEffect, useState, useCallback } from "react";
 
 export default function useRandomPokemon(initialPokemonCount: number) {
-  const [state, setState] = useState<RandomPokemonState>(() => ({
-    currentIndex: undefined,
-    indexes: indexArray(initialPokemonCount),
-    answeredIndexes: [],
-    streakIndexes: [],
-  }));
+  const [state, setState] = useState<RandomPokemonState>(() =>
+    initialRandomPokemonState(initialPokemonCount)
+  );
 
   const nextRandomPokemon = useCallback((streak: boolean) => {
     const random = Math.random();
@@ -17,27 +14,20 @@ export default function useRandomPokemon(initialPokemonCount: number) {
     if (state.currentIndex !== undefined) return;
 
     const random = Math.random();
-    setState((prev) =>
-      // NOTE: this only happens in StrictMode. Not sure if it would happen in concurrent mode.
-      prev.currentIndex !== undefined
-        ? prev
-        : { ...prev, currentIndex: Math.floor(random * prev.indexes.length) }
-    );
+    setState(startRandomPokemonState(random));
   }, [state.currentIndex]);
 
-  let pokemonIndex = undefined;
-  if (state.currentIndex !== undefined) {
-    pokemonIndex = state.indexes[state.currentIndex];
-  }
-
   return {
-    pokemonIndex,
+    pokemonIndex:
+      state.currentIndex === undefined
+        ? undefined
+        : state.indexes[state.currentIndex],
     nextRandomPokemon,
     streakCount: state.streakIndexes.length,
   };
 }
 
-type RandomPokemonState = Readonly<{
+export type RandomPokemonState = Readonly<{
   /**
    * The current index in the `indexes` array being guessed.
    */
@@ -56,7 +46,24 @@ type RandomPokemonState = Readonly<{
   streakIndexes: readonly number[];
 }>;
 
-const nextRandomPokemonState =
+export const initialRandomPokemonState = (
+  pokemonCount: number
+): RandomPokemonState => ({
+  currentIndex: undefined,
+  indexes: indexArray(pokemonCount),
+  answeredIndexes: [],
+  streakIndexes: [],
+});
+
+export const startRandomPokemonState =
+  (random: number) => (prev: RandomPokemonState) => {
+    // NOTE: currentIndex !== undefined only happens in StrictMode. Not sure if it would happen in concurrent mode.
+    return prev.currentIndex !== undefined
+      ? prev
+      : { ...prev, currentIndex: Math.floor(random * prev.indexes.length) };
+  };
+
+export const nextRandomPokemonState =
   (random: number, streak: boolean) => (prev: RandomPokemonState) => {
     let indexes = prev.indexes;
     let answeredIndexes = prev.answeredIndexes;
@@ -99,7 +106,7 @@ const nextRandomPokemonState =
     };
   };
 
-function indexArray(length: number): number[] {
+export function indexArray(length: number): number[] {
   const array = new Array<number>(length);
   for (let i = 0; i < length; i++) {
     array[i] = i;
