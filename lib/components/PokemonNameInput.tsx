5662af098115f3ee4696dc13f53de63c4267a9a6
Michael Coleman <tehcoalman@gmail.com>
2022-10-22 23:29:28 -0400

    refactor onKeyDown handling for pokemon input

--- lib/components/PokemonNameInput.tsx
+++ lib/components/PokemonNameInput.tsx
@@ -6,7 +6,14 @@
  */
 
 import clsx from "clsx";
-import { useMemo, useRef, useState, useId, useEffect } from "react";
+import {
+  useMemo,
+  useRef,
+  useState,
+  useId,
+  useEffect,
+  type AriaAttributes,
+} from "react";
 
 export type PokemonNameInputProps = {
   guessEnabled: boolean;
@@ -21,14 +28,6 @@ const PokemonNameInput = (props: PokemonNameInputProps) => {
   const { onGuess, onChange, onNextQuestion } = props;
 
   const refInput = useRef<HTMLInputElement>(null);
-
-  type InputError = {
-    state: "true" | "false" | "spelling";
-    message?: string;
-  };
-
-  const [error, setError] = useState<InputError>({ state: "false" });
-
   useEffect(() => {
     // auto focus on mount
     refInput.current?.focus();
@@ -49,36 +48,22 @@ const PokemonNameInput = (props: PokemonNameInputProps) => {
     );
   }, [props.pokemonList, props.value]);
 
-  const [selectedChoice, setSelectedChoice] = useState<number | undefined>(
-    undefined
+  const { error, resetError, checkValidName } = usePokemonNameError(
+    props.pokemonList
   );
+
+  const {
+    index: selectedChoice,
+    resetItemIndex,
+    nextItem,
+    prevItem,
+  } = useItemIndex(undefined);
 
   const inputId = useId();
   const listboxId = `${inputId}-listbox`;
   const descriptionId = `${inputId}-description`;
   const errorId = `${inputId}-error`;
   const optionId = (index: number) => `${inputId}-option${index}`;
-
-  function tryGuess(value: string) {
-    if (!props.guessEnabled) {
-      return;
-    }
-
-    if (value.trim() === "") {
-      setError({
-        state: "true",
-        message: "Entering a pokemon name is required.",
-      });
-    } else if (!props.pokemonList.includes(value)) {
-      setError({
-        state: "spelling",
-        message: "Invalid pokemon name entered.",
-      });
-    } else {
-      setSelectedChoice(undefined);
-      onGuess(value);
-    }
-  }
 
   return (
     <div>
@@ -108,10 +93,8 @@ const PokemonNameInput = (props: PokemonNameInputProps) => {
         onInput={(event) => {
           const value = event.currentTarget.value;
 
-          setError((prev) =>
-            prev.state !== "false" ? { state: "false" } : prev
-          );
-          setSelectedChoice(undefined);
+          resetError();
+          resetItemIndex();
           onChange(value);
         }}
         onKeyDown={(event) => {
@@ -123,25 +106,18 @@ const PokemonNameInput = (props: PokemonNameInputProps) => {
               let value = event.currentTarget.value;
               if (selectedChoice !== undefined) {
                 value = choices[selectedChoice];
-                setError({ state: "false" });
+                resetError();
               }
 
-              tryGuess(value);
+              if (checkValidName(value)) {
+                resetItemIndex();
+                onGuess(value);
+              }
             }
           } else if (event.key === "ArrowDown") {
-            setSelectedChoice((choice) => {
-              if (choices.length === 0) {
-                return undefined;
-              } else if (choice === undefined) {
-                return 0;
-              } else {
-                return Math.min(choice + 1, choices.length - 1);
-              }
-            });
+            nextItem(choices.length);
           } else if (event.key === "ArrowUp") {
-            setSelectedChoice((choice) =>
-              choice === undefined || choice === 0 ? undefined : choice - 1
-            );
+            prevItem();
           } else {
             preventDefault = false;
           }
@@ -172,7 +148,8 @@ const PokemonNameInput = (props: PokemonNameInputProps) => {
               aria-selected={index === selectedChoice}
               tabIndex={-1}
               onClick={() => {
-                tryGuess(pokemonName);
+                resetItemIndex();
+                onGuess(pokemonName);
                 refInput.current?.focus();
               }}
             >
@@ -190,3 +167,58 @@ const PokemonNameInput = (props: PokemonNameInputProps) => {
 };
 
 export default PokemonNameInput;
+
+type InputError = {
+  state: Extract<AriaAttributes["aria-invalid"], string>;
+  message?: string;
+};
+
+function usePokemonNameError(pokemonList: readonly string[]) {
+  const [error, setError] = useState<InputError>({ state: "false" });
+
+  return {
+    error,
+    resetError: () =>
+      setError((prev) => (prev.state !== "false" ? { state: "false" } : prev)),
+    checkValidName: (pokemonName: string): boolean => {
+      if (pokemonName.trim() === "") {
+        setError({
+          state: "true",
+          message: "Entering a pokemon name is required.",
+        });
+        return false;
+      } else if (!pokemonList.includes(pokemonName)) {
+        setError({
+          state: "spelling",
+          message: "Invalid pokemon name entered.",
+        });
+        return false;
+      } else {
+        return true;
+      }
+    },
+  };
+}
+
+function useItemIndex(initialIndex: number | undefined) {
+  const [index, setIndex] = useState(initialIndex);
+
+  return {
+    index,
+    resetItemIndex: () => setIndex(undefined),
+    nextItem: (itemsLength: number) =>
+      setIndex((index) => {
+        if (itemsLength === 0) {
+          return undefined;
+        } else if (index === undefined) {
+          return 0;
+        } else {
+          return Math.min(index + 1, itemsLength - 1);
+        }
+      }),
+    prevItem: () =>
+      setIndex((index) =>
+        index === undefined || index === 0 ? undefined : index - 1
+      ),
+  };
+}
