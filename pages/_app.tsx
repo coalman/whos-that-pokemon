74f86880bc9fd53903b895ec7f24008de264aa9e
Michael Coleman <tehcoalman@gmail.com>
2022-10-22 22:27:15 -0400

    add explanation comments for some _page files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+/**
+ * @module
+ *
+ * This adds styles/globals.css to each page.
+ *
+ * Nothing else is added in this module atm.
+ */
+
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
+/**
+ * @module
+ *
+ * Atm, this only adds some class names to the body of each page.
+ */
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head />
-      <body className=" bg-slate-900 text-slate-50">
+      <body className="bg-slate-900 text-slate-50">
         <Main />
         <NextScript />
       </body>
